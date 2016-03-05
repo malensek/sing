89658f708224e7a397b794fee2fdaaaab236f349
Matthew Malensek <matt@malensek.net>
2016-03-05 14:23:59 -0700

    Initial tests

--- src/main/java/io/sigpipe/sing/dataset/Quantizer.java
+++ src/main/java/io/sigpipe/sing/dataset/Quantizer.java
@@ -8,10 +8,33 @@ import io.sigpipe.sing.dataset.feature.Feature;
 
 public class Quantizer {
 
+    public static void main(String[] args) {
+        Quantizer q = new Quantizer(new Feature(28.3), 2.8);
+        System.out.println(q);
+
+        System.out.println(q.quantize(new Feature(1.1)));
+        System.out.println(q.quantize(new Feature(2.8)));
+        System.out.println(q.quantize(new Feature(-100.0)));
+        System.out.println(q.quantize(new Feature(80.0)));
+        System.out.println(q.quantize(new Feature(100.0)));
+
+    }
+
     private NavigableSet<Feature> ticks = new TreeSet<>();
 
-    public Quantizer() {
+    public <T> Quantizer(Feature interval, T step) {
+//        if (interval.isInterval() == false) {
+//            return;
+//        }
 
+        Double start = 0.0;
+        Double finish = 100.0;
+        Double stepSz = 2.8;
+
+        for (Double i = start; i < finish; i += stepSz) {
+            Feature f = new Feature(i);
+            insertTick(f);
+        }
     }
 
     public void insertTick(Feature tick) {
