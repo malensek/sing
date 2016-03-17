6cd017466c6b6b3f9d2f8d8c3416d7efbfdec2b1
Matthew Malensek <matt@malensek.net>
2016-03-17 14:43:28 -0600

    Add class to manage squared errors between collections

---
+++ src/main/java/io/sigpipe/sing/stat/SquaredError.java
@@ -0,0 +1,32 @@
+package io.sigpipe.sing.stat;
+
+import java.util.List;
+
+import org.apache.commons.math3.util.FastMath;
+
+import io.sigpipe.sing.dataset.feature.Feature;
+
+public class SquaredError {
+
+    private RunningStatistics sqErrs = new RunningStatistics();
+    private RunningStatistics actualStats = new RunningStatistics();
+    private RunningStatistics predictedStats = new RunningStatistics();
+
+    public SquaredError(List<Feature> actual, List<Feature> predicted) {
+        if (actual.size() != predicted.size()) {
+            throw new IllegalArgumentException(
+                    "List sizes must be equal");
+        }
+
+        for (int i = 0; i < actual.size(); ++i) {
+            Feature a = actual.get(i);
+            Feature b = predicted.get(i);
+            Feature err = a.subtract(b);
+            double p = Math.pow(err.getDouble(), 2.0);
+
+            sqErrs.put(p);
+            actualStats.put(a.getDouble());
+            predictedStats.put(b.getDouble());
+        }
+    }
+}
