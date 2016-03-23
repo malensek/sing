8cab8a06a965a0957c50b429e921cbd16bfa0dae
Matthew Malensek <matt@malensek.net>
2016-03-23 17:55:08 -0600

    Update containers for ND stats

--- src/main/java/io/sigpipe/sing/graph/ContainerEntry.java
+++ src/main/java/io/sigpipe/sing/graph/ContainerEntry.java
@@ -6,11 +6,11 @@ import io.sigpipe.sing.serialization.ByteSerializable;
 import io.sigpipe.sing.serialization.SerializationInputStream;
 import io.sigpipe.sing.serialization.SerializationOutputStream;
 import io.sigpipe.sing.stat.RunningStatistics2D;
+import io.sigpipe.sing.stat.RunningStatisticsND;
 
 public class ContainerEntry implements ByteSerializable {
-    public int feature1ID;
-    public int feature2ID;
-    public RunningStatistics2D stats;
+
+    public RunningStatisticsND stats;
 
     public ContainerEntry() {
 
@@ -19,17 +19,13 @@ public class ContainerEntry implements ByteSerializable {
     @Deserialize
     public ContainerEntry(SerializationInputStream in)
     throws IOException {
-        this.feature1ID = in.readInt();
-        this.feature2ID = in.readInt();
-        stats = new RunningStatistics2D(in);
+        //stats = new RunningStatistics2D(in);
     }
 
     @Override
     public void serialize(SerializationOutputStream out)
     throws IOException {
-        out.writeInt(feature1ID);
-        out.writeInt(feature2ID);
-        stats.serialize(out);
+        //stats.serialize(out);
     }
 
 }
--- src/main/java/io/sigpipe/sing/graph/DataContainer.java
+++ src/main/java/io/sigpipe/sing/graph/DataContainer.java
@@ -1,8 +1,6 @@
 package io.sigpipe.sing.graph;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import io.sigpipe.sing.serialization.ByteSerializable;
 import io.sigpipe.sing.serialization.SerializationInputStream;
@@ -10,10 +8,10 @@ import io.sigpipe.sing.serialization.SerializationOutputStream;
 
 public class DataContainer implements ByteSerializable {
 
-    public List<ContainerEntry> entries;
+    public ContainerEntry entry;
 
     public DataContainer() {
-        entries = new ArrayList<>();
+
     }
 
     public void merge(DataContainer container) {
@@ -27,20 +25,12 @@ public class DataContainer implements ByteSerializable {
     @Deserialize
     public DataContainer(SerializationInputStream in)
     throws IOException {
-        int numEntries = in.readInt();
-        this.entries = new ArrayList<>(numEntries);
-        for (int i = 0; i < numEntries; ++i) {
-            ContainerEntry entry = new ContainerEntry(in);
-            entries.add(entry);
-        }
+
     }
 
     @Override
     public void serialize(SerializationOutputStream out)
     throws IOException {
-        out.writeInt(entries.size());
-        for (ContainerEntry entry : entries) {
-            entry.serialize(out);
-        }
+
     }
 }
