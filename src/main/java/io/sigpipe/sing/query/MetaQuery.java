be8fcc2e915ae339af14c8cd6a8a632b4efc8ec3
Matthew Malensek <matt@malensek.net>
2016-03-28 20:13:22 -0600

    Initial serialization implementation
    Likely has bugs...

--- src/main/java/io/sigpipe/sing/query/MetaQuery.java
+++ src/main/java/io/sigpipe/sing/query/MetaQuery.java
@@ -6,13 +6,13 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.NavigableMap;
 import java.util.Set;
 
 import io.sigpipe.sing.dataset.feature.Feature;
 import io.sigpipe.sing.dataset.feature.FeatureType;
 import io.sigpipe.sing.graph.DataContainer;
 import io.sigpipe.sing.graph.Vertex;
+import io.sigpipe.sing.serialization.SerializationException;
 import io.sigpipe.sing.serialization.SerializationInputStream;
 import io.sigpipe.sing.serialization.SerializationOutputStream;
 
@@ -171,14 +171,31 @@ public class MetaQuery implements Query {
 
     @Deserialize
     public MetaQuery(SerializationInputStream in)
-    throws IOException {
-
+    throws IOException, SerializationException {
+        int size = in.readInt();
+        expressions = new HashMap<>(size);
+        for (int i = 0; i < size; ++i) {
+            int listSize = in.readInt();
+            List<Expression> expList = new ArrayList<>(listSize);
+            for (int j = 0; j < listSize; ++j) {
+                Expression exp = new Expression(in);
+                expList.add(exp);
+            }
+            String featureName = expList.get(0).getOperand().getName();
+            expressions.put(featureName, expList);
+        }
     }
 
     @Override
     public void serialize(SerializationOutputStream out)
     throws IOException {
-
+        out.writeInt(expressions.size());
+        for (List<Expression> expList : expressions.values()) {
+            out.writeInt(expList.size());
+            for (Expression expression : expList) {
+                expression.serialize(out);
+            }
+        }
     }
 
 }
