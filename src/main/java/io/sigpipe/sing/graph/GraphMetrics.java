2c1440e879c9ad2c4440928fecb9a344d3d81601
Matthew Malensek <matt@malensek.net>
2016-04-08 23:42:09 -0600

    Implement equals()

--- src/main/java/io/sigpipe/sing/graph/GraphMetrics.java
+++ src/main/java/io/sigpipe/sing/graph/GraphMetrics.java
@@ -13,6 +13,25 @@ public class GraphMetrics implements Cloneable {
     public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        GraphMetrics that = (GraphMetrics) obj;
+        return this.vertices == that.vertices
+            && this.leaves == that.leaves;
+    }
+
     public GraphMetrics(int vertices, int leaves) {
         this.vertices = vertices;
         this.leaves = leaves;
