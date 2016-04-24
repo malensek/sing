6566949470a7c5101775b41616cd723d60b7c84b
Matthew Malensek <matt@malensek.net>
2016-04-23 22:13:16 -0600

    Update Sketch creation test harness

--- src/main/java/io/sigpipe/sing/adapters/ReadMetaBlob.java
+++ src/main/java/io/sigpipe/sing/adapters/ReadMetaBlob.java
@@ -43,7 +43,108 @@ public class ReadMetaBlob {
         fh.addFeature("location", FeatureType.STRING);
         Sketch s = new Sketch(fh);
 
-        FeatureSurvey fs = new FeatureSurvey();
+        loadData(args[0], s);
+
+        System.gc();
+        System.gc();
+        
+        System.out.println();
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println("max=" + runtime.maxMemory());
+        System.out.println("total=" + runtime.totalMemory());
+        System.out.println("free=" + runtime.freeMemory());
+        System.out.println("used=" + (runtime.totalMemory() - runtime.freeMemory()));
+        System.out.println("estimate=" + estimateMemoryUsage(
+                    s,
+                    s.getMetrics().getVertexCount(),
+                    s.getMetrics().getLeafCount()));
+
+        scan.nextInt();
+
+        PerformanceTimer info = new PerformanceTimer("info");
+        info.start();
+        System.out.println(s.getRoot().numLeaves());
+        System.out.println(s.getRoot().numDescendants());
+        System.out.println(s.getRoot().numDescendantEdges());
+        info.stopAndPrint();
+        System.out.println(s.getMetrics());
+
+        SerializationOutputStream out = new SerializationOutputStream(
+                new GZIPOutputStream(
+                new FileOutputStream(new File("d.bin"))));
+        //s.getRoot().find(new Feature("location", 319920l));
+        PartitionQuery pq = new PartitionQuery(s.getMetrics());
+//        rq.addExpression(
+//                new Expression(
+//                    Operator.RANGE_INC, new Feature("temperature_surface", 260.0f), new Feature(300.0f)));
+        String removePrefix = "d";
+        pq.addExpression(
+                new Expression(
+                    Operator.STR_PREFIX, new Feature("location", removePrefix)));
+
+        PerformanceTimer exec = new PerformanceTimer("exec");
+        exec.start();
+        pq.execute(s.getRoot());
+        pq.serializeResults(s.getRoot(), out);
+        out.close();
+        exec.stopAndPrint();
+
+        s.geoTrie.remove(removePrefix);
+
+        System.gc();
+        System.gc();
+        System.out.println();
+        System.out.println("max=" + runtime.maxMemory());
+        System.out.println("total=" + runtime.totalMemory());
+        System.out.println("free=" + runtime.freeMemory());
+        System.out.println("used=" + (runtime.totalMemory() - runtime.freeMemory()));
+        System.out.println("estimate=" + estimateMemoryUsage(
+                    s,
+                    s.getMetrics().getVertexCount(),
+                    s.getMetrics().getLeafCount()));
+
+
+        info.start();
+        System.out.println(s.getRoot().numLeaves());
+        System.out.println(s.getRoot().numDescendants());
+        System.out.println(s.getRoot().numDescendantEdges());
+        info.stopAndPrint();
+
+
+        scan.nextInt();
+
+        System.out.println(s.getMetrics());
+
+        System.out.println(s.geoTrie.query("dj").b);
+
+        loadData(args[1], s);
+        System.gc();
+        System.gc();
+        System.out.println();
+        System.out.println("max=" + runtime.maxMemory());
+        System.out.println("total=" + runtime.totalMemory());
+        System.out.println("free=" + runtime.freeMemory());
+        System.out.println("used=" + (runtime.totalMemory() - runtime.freeMemory()));
+        System.out.println("estimate=" + estimateMemoryUsage(
+                    s,
+                    s.getMetrics().getVertexCount(),
+                    s.getMetrics().getLeafCount()));
+
+
+    }
+
+    private static void loadData(String fileName, Sketch s)
+    throws Exception {
+        System.out.println("Reading metadata blob: " + fileName);
+        FileInputStream fIn = new FileInputStream(fileName);
+        BufferedInputStream bIn = new BufferedInputStream(fIn);
+        SerializationInputStream in = new SerializationInputStream(bIn);
+
+        int num = in.readInt();
+        System.out.println("Records: " + num);
+
+        PerformanceTimer addAllPaths = new PerformanceTimer("addAllPaths");
+        addAllPaths.start();
         for (int i = 0; i < num; ++i) {
             float lat = in.readFloat();
             float lon = in.readFloat();
