87627b0764cc87fa21d9a3f817245e92395a1121
Matthew Malensek <matt@malensek.net>
2016-07-07 11:42:11 -0600

    Add keys array for storing random keys

--- src/main/java/io/sigpipe/sing/stat/ReservoirSampler.java
+++ src/main/java/io/sigpipe/sing/stat/ReservoirSampler.java
@@ -1,11 +1,17 @@
 package io.sigpipe.sing.stat;
 
+import java.util.Random;
+
 public class ReservoirSampler {
 
     private int count;
     private double[] reservoir;
+    private double[] keys;
+    private Random random = new Random();
+
     public ReservoirSampler(int size) {
         reservoir = new double[size];
+        keys = new double[size];
     }
     }
     public int size() {
