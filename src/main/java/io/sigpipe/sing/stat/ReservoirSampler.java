9d1a01c0f118521d7d6133330467818afe9247c1
Matthew Malensek <matt@malensek.net>
2016-07-07 11:41:42 -0600

    Add constructors and simple getters

--- src/main/java/io/sigpipe/sing/stat/ReservoirSampler.java
+++ src/main/java/io/sigpipe/sing/stat/ReservoirSampler.java
@@ -1,4 +1,17 @@
 package io.sigpipe.sing.stat;
 
 public class ReservoirSampler {
+
+    private int count;
+    private double[] reservoir;
+    public ReservoirSampler(int size) {
+        reservoir = new double[size];
+    }
+    }
+    public int size() {
+        return reservoir.length;
+    }
+    public double[] samples() {
+        return this.reservoir;
+    }
 }
